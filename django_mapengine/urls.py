31bbb9be749429911685fb5fbd07732b6500357c
RL-INSTITUT\hendrik.huyskens <hendrik.huyskens@rl-institut.de>
2023-03-22 09:34:02 +0100

    Fix distill urls (not from registry)

--- django_mapengine/urls.py
+++ django_mapengine/urls.py
@@ -1,25 +1,23 @@
+"""URLs for MVTs and cluster geojsons"""
 
 from django.conf import settings
 from django.urls import path
 from django_distill import distill_path
 from djgeojson.views import GeoJSONLayerView
 
-from . import views, distill, mvt, setup
+from . import distill, mvt
 
 app_name = "django_mapengine"
 
 urlpatterns = [
-    path(
-        f"clusters/{cluster.layer_id}.geojson",
-        GeoJSONLayerView.as_view(model=cluster.model)
-    )
+    path(f"clusters/{cluster.layer_id}.geojson", GeoJSONLayerView.as_view(model=cluster.model))
     for cluster in settings.MAP_ENGINE_API_CLUSTERS
 ]
 
 urlpatterns += [
     path(
         f"{name}_mvt/<int:z>/<int:x>/<int:y>/",
-        mvt.mvt_view_factory(name, [mvt.MVTLayer(mvt_api.layer_id, queryset=mvt_api.manager) for mvt_api in mvt_apis])
+        mvt.mvt_view_factory(name, [mvt.MVTLayer(mvt_api.layer_id, queryset=mvt_api.manager) for mvt_api in mvt_apis]),
     )
     for name, mvt_apis in settings.MAP_ENGINE_API_MVTS.items()
 ]
@@ -29,10 +27,12 @@ if settings.MAP_ENGINE_DISTILL:
     urlpatterns += [
         distill_path(
             f"<int:z>/<int:x>/<int:y>/{name}.mvt",
-            mvt.mvt_view_factory(name, layers),
+            mvt.mvt_view_factory(
+                name, [mvt.MVTLayer(mvt_api.layer_id, queryset=mvt_api.manager) for mvt_api in mvt_apis]
+            ),
             name=name,
             distill_func=distill.get_all_statics_for_state_lod,
             distill_status_codes=(200, 204, 400),
         )
-        for name, layers in registry.mvt_registry.items()
+        for name, mvt_apis in settings.MAP_ENGINE_API_MVTS.items()
     ]
